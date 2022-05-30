55d640071647b4c2d0ce468d02e8c0e00953d2ae
ioannakandi <ioannakandi@gmail.com>
2022-05-30 12:44:30 +0300

    generated graph plot (countries)

--- graphAnalysis.py
+++ graphAnalysis.py
@@ -7,6 +7,7 @@ import os
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 from scipy.spatial.distance import pdist, squareform
+import networkx as nx
 
 #---------importing the dataset & isolating the columns we want to use-----------
 old_df = pd.read_csv("data.csv", usecols=["ReportingCountry", "NumberDosesReceived", "TargetGroup", "Vaccine"])
@@ -21,6 +22,7 @@ df=df.dropna()
 
 #------creating a new dataframe using a list icluding unique country codes & NumberDosesReceived------
 countries=df['ReportingCountry'].unique()
+print(countries)
 
 numberDosesReceived= []
 
@@ -41,7 +43,25 @@ updated_df.insert(1, 'AscCountries', range(0, len(updated_df)))
 #correlation_df = updated_df.corr()
 #print(correlation_df)
 
-#---------calculating nodes ------------
+#---------calculating distance between countries (nodes) ------------
 distances = pdist(updated_df[['AscCountries','NumberDosesReceived']].values, metric='euclidean')
 dist_matrix = squareform(distances)
-print(dist_matrix)
+array_df= pd.DataFrame(dist_matrix.astype(int)) #affinity matrix
+array_df_stacked=array_df.stack()
+array_df_stacked.to_csv('array_df.csv',index=True)
+#array_df.iloc[:, 0:29].agg(lambda x: ','.join(x.values), axis=1).T
+#array_df_1= array_df.iloc[:, 0:29]
+print(array_df_stacked)
+read_array_df= pd.read_csv('stacked_data.txt')
+#read_array_df.loc[read_array_df['Source']==0 ,'Source']='AT'
+
+#---------------------------------plotting the graph----------------------------------------
+G=nx.from_pandas_edgelist(read_array_df, source='Source',target='Target',edge_attr='Weight')
+# Plot it
+#nx.draw(G, with_labels=True, node_color='pink', edge_color='#00A36C')
+mapping = {0: "AT", 1: "BE", 2: "BG", 3:'CY', 4:'CZ', 5:'DE', 6:'DK', 7:'EE', 8:'EL', 9:'ES', 10:'FI', 11:'FR',
+           12:'HR', 13:'HU', 14:'IE', 15:'IS', 16:'IT', 17:'LI', 18:'LT', 19:'LU', 20:'LV', 21:'NL', 22:'NO',
+           23:'PL', 24:'PT', 25:'RO', 26:'SE', 27:'SI', 28:'SK' }
+H = nx.relabel_nodes(G, mapping)
+nx.draw(H, with_labels=True, node_color='pink', edge_color='#6495ED')
+plt.show()
