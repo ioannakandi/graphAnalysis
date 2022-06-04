5503ac81285caf7585a6da4f29bfaaf5d5ae072b
Kostis Mavrogiorgos <63185922+mavrogiorgos@users.noreply.github.com>
2022-06-04 18:55:57 +0300

    Add documentation.

--- graphAnalysis.py
+++ graphAnalysis.py
@@ -33,11 +33,12 @@ for country in countries :
     numberDosesReceived.append(doses) 
      
 updated_df = pd.DataFrame(list(zip(countries, numberDosesReceived)), columns =['ReportingCountry', 'NumberDosesReceived'])
-#print(updated_df)
+print(updated_df)
 
 #---------inserting AscCountries column (num) for country codes (str) in ascending order ------------
-updated_df.insert(1, 'AscCountries', range(0, len(updated_df)))
+updated_df.insert(1, 'AscCountries', range(1, int(len(updated_df))+1))
 #updated_df[['AscCountries','NumberDosesReceived']]
+print(updated_df)
 
 #similarity=pd.DataFrame(1 - squareform(pdist(updated_df.set_index('ReportingCountry'), lambda u,v: (u != v).mean())))
 #print(similarity)
@@ -48,22 +49,26 @@ updated_df.insert(1, 'AscCountries', range(0, len(updated_df)))
 #---------calculating distance between countries (nodes) ------------
 distances = pdist(updated_df[['AscCountries','NumberDosesReceived']].values, metric='euclidean')
 dist_matrix = squareform(distances)
+print(dist_matrix)
 array_df= pd.DataFrame(dist_matrix.astype(int)) #affinity matrix
+array_df.index = np.arange(1, len(array_df)+1)
+array_df.columns = array_df.index
+print(array_df)
 array_df_stacked=array_df.stack()
 array_df_stacked.to_csv('array_df.csv',index=True)
 #array_df.iloc[:, 0:29].agg(lambda x: ','.join(x.values), axis=1).T
 #array_df_1= array_df.iloc[:, 0:29]
 print(array_df_stacked)
-read_array_df= pd.read_csv('stacked_data.txt')
+read_array_df= pd.read_csv('stacked_data_new.txt')
 #read_array_df.loc[read_array_df['Source']==0 ,'Source']='AT'
 
 #---------------------------------plotting the graph----------------------------------------
 G=nx.from_pandas_edgelist(read_array_df, source='Source',target='Target',edge_attr='Weight')
 # Plot it
 #nx.draw(G, with_labels=True, node_color='pink', edge_color='#00A36C')
-mapping = {0: "AT", 1: "BE", 2: "BG", 3:'CY', 4:'CZ', 5:'DE', 6:'DK', 7:'EE', 8:'EL', 9:'ES', 10:'FI', 11:'FR',
-           12:'HR', 13:'HU', 14:'IE', 15:'IS', 16:'IT', 17:'LI', 18:'LT', 19:'LU', 20:'LV', 21:'NL', 22:'NO',
-           23:'PL', 24:'PT', 25:'RO', 26:'SE', 27:'SI', 28:'SK' }
+mapping = {1: "AT", 2: "BE", 3: "BG", 4:'CY', 5:'CZ', 6:'DE', 7:'DK', 8:'EE', 9:'EL', 10:'ES', 11:'FI', 12:'FR',
+           13:'HR', 14:'HU', 15:'IE', 16:'IS', 17:'IT', 18:'LI', 19:'LT', 20:'LU', 21:'LV', 22:'NL', 23:'NO',
+           24:'PL', 25:'PT', 26:'RO', 27:'SE', 28:'SI', 29:'SK' }
 H = nx.relabel_nodes(G, mapping)
 plt.title("Pfizer Vaccination Graph of EU Countries")
 nx.draw(H, with_labels=True, node_color='pink', edge_color='#6495ED')
